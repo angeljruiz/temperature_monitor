b7d7b1bdd74e619c2ce2a2e453c2de2c66bb0207
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-11-14 07:24:42 -0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 let noble = require('noble');
 
 noble.on('discover', (p) => {
-  console.log(p.advertisement.localName)
+  console.log({ name: p.advertisement.localName, id: p.uuid })
   console.log()
-  if (p.uuid === 'e868e71ae7ba' || p.advertisement.localName === 'ESP32test') {
+  if (p.uuid === 'e868e71ae7ba' || p.advertisement.localName === 'DFRobot_ESP32') {
 
-    p.once('connect', () => console.log('connected'))
+    p.once('connect', () => {
+      p.discoverAllServicesAndCharacteristics((err, services, characteristics) => {
+        console.log({err, services, characteristics});
+      })
+    })
     p.connect()
   }
 })
