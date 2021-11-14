6a7317faba8c392dcadc62ed741caa575045d333
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-11-13 23:53:48 -0700

    update

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let noble = require('noble');
 
-noble.on('discover', (p) => {console.log(p); console.log()})
+noble.on('discover', (p) => {
+  if (p.uuid === 'e868e71ae7ba') {
+    console.log(p)
+    p.connect()
+  }
+})
+
 noble.on('stateChange', state => {
   console.log(state)
 
-  if (state === 'poweredOn') noble.startScanning(['e868e71ae7ba'])
+  if (state === 'poweredOn') noble.startScanning()
 })
 
 
