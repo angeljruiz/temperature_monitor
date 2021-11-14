921e3d592ad9aefdf263e8d1d7cf277fd2f13996
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-11-13 23:55:44 -0700

    update

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let noble = require('noble');
 noble.on('discover', (p) => {
   if (p.uuid === 'e868e71ae7ba') {
     console.log(p)
+
+    p.once('connect', () => console.log('connected'))
     p.connect()
   }
 })
