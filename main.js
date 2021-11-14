878bbe54279aaf8eda2904d61022ce7ba7dcf594
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-11-13 20:43:53 -0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let noble = require('noble');
 
-noble.on('discover', (p) => console.log(p + "\n"))
+noble.on('discover', (p) => {console.log(p); console.log()})
 noble.on('stateChange', state => {
   console.log(state)
 
